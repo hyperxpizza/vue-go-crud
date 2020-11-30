ed1834a4c3e3ecfb78939efbbcbe8f8acabe4196
hyperxpizza <wojtek.frackowski@gmail.com>
2020-11-30 23:34:31 +0100

    update client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app" class="small-container">
-        <h1>Employees</h1>
+        <h1>Vue.js Golang CRUD example</h1>
         
         <employee-form @add:employee="addEmployee"/>
         <employee-table 
@@ -30,27 +30,51 @@ export default {
         this.getEmployees()
     },
     methods: {
-        addEmployee(employee) {
+        async addEmployee(employee){
             const lastId =
-                this.employees.length > 0
-                ? this.employees[this.employees.length - 1].id
-                : 0;
-  
+            this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
             const id = lastId + 1;
-            const newEmployee = { ...employee, id };
 
-            this.employees = [...this.employees, newEmployee];
+            employee.id = id;
+
+            try {
+                const response = await fetch('http://localhost:8081/api/user',{
+                    method: 'POST',
+                    body: JSON.stringify(employee),
+                    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                });
+
+                const data = await response.json()
+                this.employees = [...this.employees, data]
+            } catch(error){
+                console.error(error)
+            }
         },
-        deleteEmployee(id){
-            this.employees = this.employees.filter(
-                employee => employee.id !== id
-            )
+        async deleteEmployee(id){
+           try{
+               await fetch(`http://localhost:8081/api/delete/${id}`, {
+                   method: 'DELETE'
+               });
+               this.employees = this.employees.filter(employee => employee.id !== id);
+            } catch(error){
+                console.error(error);
+            }
         },
-        editEmployee(id, updatedEmployee){
-            this.employees = this.employees.map(employee =>
-                employee.id === id ? updatedEmployee : employee
-            )
-        },
+        async editEmployee(id, updatedEmployee){
+            try {
+                const response = await fetch(`http://localhost:8081/api/update`,{
+                    method: "PUT",
+                    body: JSON.stringify(updatedEmployee),
+                    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                })
+                const data = await response.json()
+                this.employees = this.employees.map(employee => employee.id === id ? data : employee)
+            } catch(error){
+                console.error(error)
+            }
+        },  
         async getEmployees(){
             try {
                 const response = await fetch('http://localhost:8081/api/users')
