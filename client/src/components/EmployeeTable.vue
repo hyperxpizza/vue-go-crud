49d26333ab6333f80da87f677c1258b5a4cf6bc9
hyperxpizza <wojtek.frackowski@gmail.com>
2020-11-30 20:44:27 +0100

    add client

---
+++ client/src/components/EmployeeTable.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="employee-table">
+        <table>
+            <p v-if="employees.length < 1" class="empty-table">No employees</p>
+            <thead v-else>
+                <tr>
+                    <th>Employee name</th>
+                    <th>Employee email</th>
+                </tr>
+            </thead>
+            <tbody>
+               <tr v-for="employee in employees" :key="employee.id">
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.name" />
+                    </td>
+                    <td v-else>{{ employee.name }}</td>
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.email">
+                    </td>
+                    <td v-else>{{ employee.email }}</td>
+                    <td v-if="editing === employee.id">
+                        <button @click="editEmployee(employee)">Save</button>
+                        <button @click="cancelEdit(employee)" class="muted-button">Cancel</button>
+                    </td>
+                    <td v-else>
+                       <button @click="editMode(employee.id)">Edit</button>
+                       <button @click="$emit('delete:employee', employee.id)">Delete</button>
+                   </td>
+               </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'employee-table',
+    props: {
+        employees: Array
+    },
+    data(){
+        return {
+            editing: null,
+        }
+    },
+    mounted(){
+        this.getEmployees()
+    },
+    methods: {
+        editMode(employee) {
+            this.cachedEmployee = Object.assign({}, employee)
+            this.editing = employee.id
+        },
+        cancelEdit(employee){
+            Object.assign(employee, this.cachedEmployee)
+            this.editing = null;
+        },
+        editEmployee(employee){
+            if (employee.name === '' || employee.email === '') return
+            this.$emit('edit:employee', employee.id, employee)
+            this.editing = null
+        },
+        async getEmployees(){
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users')
+                const data = await response.json()
+                this.employees = data
+            } catch (error){
+                console.error(error)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        margin: 0 0.5rem 0 0;
+    }
+</style>
